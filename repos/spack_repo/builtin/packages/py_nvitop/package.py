8f471148e3f639463e5245c49acfdf16ea6f61e0
Niklas Bölter <niklas.boelter@gwdg.de>
2026-09-03 09:40:24 +0200

    py-nvitop: add v1.5.3->v1.7.1

--- repos/spack_repo/builtin/packages/py_nvitop/package.py
+++ repos/spack_repo/builtin/packages/py_nvitop/package.py
@@ -20,12 +20,19 @@ class PyNvitop(PythonPackage):
 
     license("Apache-2.0", checked_by="nboelte")
 
+    version("1.7.1", sha256="3803112a1d4a7e01989ae5bf5a5e42f0366bf791d68ad8e3b1859d7ed747140c")
+    version("1.6.2", sha256="267b341b66ac3819f116ad11819f4da2e121fe2be1dde979026b6526966211e9")
+    version("1.5.3", sha256="ab50fbcfb986607d36f4dd07e124cb5582290d7f7efb2e1d97635b4487fac1fb")
     version("1.4.0", sha256="92f313e9bd89fe1a9d54054e92f490f34331f1b7847a89ddaffd6a7fde1437bb")
 
-    depends_on("py-nvidia-ml-py@11.450.51:12.561", type=("build", "run"))
+    depends_on("py-nvidia-ml-py@11.450.51:13.596.0a0", type=("build", "run"), when="@1.7.1")
+    depends_on("py-nvidia-ml-py@11.450.51:13.591.0a0", type=("build", "run"), when="@1.6.2")
+    depends_on("py-nvidia-ml-py@11.450.51:13.581.0a0", type=("build", "run"), when="@1.5.3")
+    depends_on("py-nvidia-ml-py@11.450.51:12.561.0a0", type=("build", "run"), when="@1.4.0")
     depends_on("py-psutil@5.6.6:", type=("build", "run"))
-    depends_on("py-cachetools@1.0.1:", type=("build", "run"))
-    depends_on("py-termcolor@1.0.0:", type=("build", "run"))
+    # The dependencies cachetools and termcolor were vendored in 1.4.2
+    depends_on("py-cachetools@1.0.1:", type=("build", "run"), when="@:1.4.1")
+    depends_on("py-termcolor@1.0.0:", type=("build", "run"), when="@:1.4.1")
     depends_on("python@3.7:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
 
